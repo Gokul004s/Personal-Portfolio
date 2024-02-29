59a5c268cae7fea1bcb09736ea28ea432962be30
Gokul <140362819+Gokul004s@users.noreply.github.com>
2024-02-29 15:58:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+var togglebtn=document.querySelector(".togglebtn");
+var nav=document.querySelector(".navlinks");
+var links=document.querySelector(".navlinks li");
+
+togglebtn.addEventListener("click", function(){
+    this.classList.toggle("click");
+    nav.classList.toggle("open");
+})
+
+var typed=new Typed(".input",{
+    strings:["Frontend Developer","Web Developer"],
+    typedSpeed:50,
+    backSpeed:50,
+    loop:true
+})
+
+window.addEventListener("scroll",function()
+{
+    var header=document.querySelector("header");
+    header.classList.toggle("sticky",window.scrollY > 0 )
+})
 function showSidebar(){
     const sidebar=document.querySelector('.side-bar')
     sidebar.style.display='flex'
